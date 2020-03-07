1cfd60af98d3822c728fbc560750bd5194e0e0b0
Justin Galang <justingalang@gmail.com>
2020-03-07 01:24:26 -0600

    Initial Commit

--- FlashcardWeek4/ViewController.swift
+++ FlashcardWeek4/ViewController.swift
@@ -9,10 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /*
+        //card
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15
+        card.layer.shadowOpacity = 0.35
+
+        //labels
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        */
+ 
+    }
+    
+    
+    func updateFlashcard(question: String, answer: String) {
+        
+    }
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if (frontLabel.isHidden == true)
+        {
+            frontLabel.isHidden = false
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
     }
 
 
